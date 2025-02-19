b158f5253c9c850247db5d9a358dd97ab7ac7318
M Dev - 114 <autocad2207271@gmail.com>
2025-02-19 14:54:32 +0530

    Create settings.py

---
+++ image_caption_generator/settings.py
@@ -0,0 +1,22 @@
+INSTALLED_APPS = [
+    # ...
+    'captions',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        # ... other settings ...
+    },
+]
